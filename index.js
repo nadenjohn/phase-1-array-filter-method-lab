1d58d94ae96ce3b1918f523876d4a2098b6a5ecd
John <nadenjohn@gmail.com>
2022-03-01 09:28:00 -0800

    commit tree 60cd0dcb4620d36f1d1eda2c5d363caf9384c547

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, str) {
+    const result = array.filter(element => element.toLowerCase() === str.toLowerCase())
+        return result
+}
+
+function fuzzyMatch(array, letter) {
+    const match = array.filter(element => element.startsWith(letter))
+    return match
+}
+
+function matchName(array, str) {
+    const nameMatch = array.filter(element =>  element.name === str)
+    return nameMatch
+}
